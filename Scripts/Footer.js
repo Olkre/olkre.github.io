3f2961e780158132293ac0eb2d8e54db4640d7ee
Alexander <oleksandr.kre@outlook.com>
2021-05-08 13:21:11 +0300

    Workplace Uploaded

---
+++ Scripts/Footer.js
@@ -0,0 +1,5 @@
+const footer =Vue.createApp({
+    template:'<footer class="footer-js container d-flex justify-content-between align-items-center"><h3>Olkre</h3><div class=""><a href=#>Dribble</a></div><p>2021</p></footer>'
+})
+
+footer.mount("#footer")
