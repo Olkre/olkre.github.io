c89cacd21103fafc9e1f1688926ca73daa3d9c61
Olkre <81689571+Olkre@users.noreply.github.com>
2025-04-03 19:08:03 -0400

    added wr logo scaling block

--- Scripts/code.js
+++ Scripts/code.js
@@ -1,30 +1,30 @@
-var StickyElement = function(node){
-  var doc = $(document), 
-      fixed = false,
-      anchor = node.find('.sticky-anchor'),
-      content = node.find('.sticky-content');
-  
-  var onScroll = function(e){
+var StickyElement = function (node) {
+  var doc = $(document),
+    fixed = false,
+    anchor = node.find(".sticky-anchor"),
+    content = node.find(".sticky-content");
+
+  var onScroll = function (e) {
     var docTop = doc.scrollTop(),
-        anchorTop = anchor.offset().top;
-    
-    console.log('scroll', docTop, anchorTop);
-    if(docTop > anchorTop){
-      if(!fixed){
+      anchorTop = anchor.offset().top;
+
+    console.log("scroll", docTop, anchorTop);
+    if (docTop > anchorTop) {
+      if (!fixed) {
         anchor.height(content.outerHeight());
-        content.addClass('fixed');        
+        content.addClass("fixed");
         fixed = true;
       }
-    }  else   {
-      if(fixed){
+    } else {
+      if (fixed) {
         anchor.height(0);
-        content.removeClass('fixed'); 
+        content.removeClass("fixed");
         fixed = false;
       }
     }
   };
-  
-  $(window).on('scroll', onScroll);
+
+  $(window).on("scroll", onScroll);
 };
 
-var demo = new StickyElement($('#sticky'));
+var demo = new StickyElement($("#sticky"));
